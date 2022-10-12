46079278bb9a0f0cd06aeffa110add991261cb25
Jagan-97 <jagan.psaran38@gmail.com>
2022-10-12 06:43:06 +0530

    Added WagesForMonth Logic

--- src/EmployeeSalary.java
+++ src/EmployeeSalary.java
@@ -8,6 +8,8 @@ public class EmployeeSalary {
     public static int empRatePerHr = 20 ;
     public static int empHrs = 8 ;
     public static int salary;
+    public static int numOfworkingDay=20;
+    public static int  totalSalary=0;
     public static int x = random.nextInt(2);
 
      public static void EmployeePresent(){
@@ -63,6 +65,27 @@ public class EmployeeSalary {
                 salary=empRatePerHr * empHrs;
                 System.out.println("Employee Salary=" +salary);
         }
+    }
+
+    public static void WagesForMonth(){
+        for ( int day=1; day<=numOfworkingDay; day++)
+        {
+            switch (x) {
+                case 1:
+                    empHrs=8;
+                    break;
+                case 2:
+                    empHrs=16;
+                    break;
+                default:
+                    empHrs=0;
+            }
+            salary=empHrs*empRatePerHr;
+            totalSalary=totalSalary+salary;
+            System.out.println("RandomCheck= " +x);
+            System.out.println("Salary= " +salary);
+        }
+        System.out.println("Totalsalary= " +totalSalary);
     }
 
 }
