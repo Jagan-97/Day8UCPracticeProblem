5f4f89d12e9090eefc3dc2e1bb78029bb511815d
Jagan-97 <jagan.psaran38@gmail.com>
2022-10-12 06:10:31 +0530

    Added Dailywage logic

--- src/EmployeeMain.java
+++ src/EmployeeMain.java
@@ -3,5 +3,6 @@ import java.util.Random;
 public class EmployeeMain extends EmployeeSalary {
     public static void main(String args[]){
         EmployeePresent();
+        DailyWage();
     }
 }
--- src/EmployeeSalary.java
+++ src/EmployeeSalary.java
@@ -2,7 +2,12 @@ import java.util.Random;
 
 public class EmployeeSalary {
     public static Random random = new Random();
+    public static int isPresent = 1;
+    public static int empRatePerHr = 20 ;
+    public static int empHrs = 8 ;
+    public static int salary;
     public static int x = random.nextInt(2);
+
      public static void EmployeePresent(){
          System.out.println(x);
          if(x>0.5) {
@@ -11,4 +16,18 @@ public class EmployeeSalary {
              System.out.println("Employee is absent");
          }
      }
+    public static void DailyWage(){
+
+        if (isPresent == x )
+        {
+            salary=empRatePerHr * empHrs ;
+            System.out.println("DailyWage is " +salary);
+        }
+        else{
+            salary = 0;
+            System.out.println("DailyWage is " +salary);
+        }
+    }
+
+
 }
